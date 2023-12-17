87697c228701a82487d39b098e4fc80fc0a4f16e
Shahrukh khan <shahrukhkhan9912@gmail.com>
2023-12-17 17:40:40 +0530

    Create CRUD App

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './App.css';
+// import Button from 'react-bootstrap/Button';
+
+const App = () => {
+  return (
+    <div className="App">
+
+
+
+
+
+      {/* <Button variant="primary">Primary</Button>{' '}
+      <Button variant="secondary">Secondary</Button>{' '}
+      <Button variant="success">Success</Button>{' '}
+      <Button variant="warning">Warning</Button>{' '}
+      <Button variant="danger">Danger</Button>{' '}
+      <Button variant="info">Info</Button>{' '}
+      <Button variant="light">Light</Button>{' '} */}
+    </div>
+  )
+}
+
+export default App
